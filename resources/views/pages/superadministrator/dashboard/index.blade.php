6adf24b9133e6a58f4232c678fb8a6a5b0e25c23
monciego <jerichobantiquete@gmail.com>
2024-06-12 21:30:26 +0800

    feat: stat-cards

--- resources/views/pages/superadministrator/dashboard/index.blade.php
+++ resources/views/pages/superadministrator/dashboard/index.blade.php
@@ -1,8 +1,8 @@
 <x-app-layout>
-
     @include('components.welcome-banner')
     <div class="grid grid-cols-12 gap-6">
-        {{-- @include('pages.admin.dashboard.stat-cards') --}}
+        <x-stat-cards title="Today's Violations" count="0" />
+        <x-stat-cards title="Total Violations" count="0" />
+        <x-stat-cards title="Monthly Violations" count="0" />
     </div>
-
 </x-app-layout>
