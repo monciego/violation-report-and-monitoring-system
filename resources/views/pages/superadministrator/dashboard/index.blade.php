9813b6d68bb394ec444b5a5375239a832244dfe7
monciego <jerichobantiquete@gmail.com>
2024-06-12 21:20:40 +0800

    feat: welcome banner

--- resources/views/pages/superadministrator/dashboard/index.blade.php
+++ resources/views/pages/superadministrator/dashboard/index.blade.php
@@ -1,17 +1,8 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('dashboard') }}
-        </h2>
-    </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("you're logged in as superadmin!") }}
-                </div>
-            </div>
-        </div>
+    @include('components.welcome-banner')
+    <div class="grid grid-cols-12 gap-6">
+        {{-- @include('pages.admin.dashboard.stat-cards') --}}
     </div>
+
 </x-app-layout>
