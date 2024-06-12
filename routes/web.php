98a7bb5a22e51f025408f9fbf7e7c59299814779
monciego <jerichobantiquete@gmail.com>
2024-06-12 17:05:45 +0800

    feat: login page and change register middleware

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,20 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::group(['middleware' => ['auth', 'verified']], function() {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
+});
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
