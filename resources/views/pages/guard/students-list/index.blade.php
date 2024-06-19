1f93027c887d0da15ba6e2164382e2293eff0914
monciego <jerichobantiquete@gmail.com>
2024-06-19 21:30:15 +0800

    feat: hide the register violation in superadmin account

--- resources/views/pages/guard/students-list/index.blade.php
+++ resources/views/pages/guard/students-list/index.blade.php
@@ -13,13 +13,19 @@
                 <span class="text-sm text-gray-100">{{ $student->student_id }}</span>
                 <span class="text-sm text-gray-400">{{ $student->department }}</span>
                 <div class="flex mt-4 md:mt-6">
+                    @if (Auth::user()->hasRole("superadministrator"))
+                    <a href="#"
+                        class="inline-flex items-center px-4 py-2 text-xs font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
+                        Show more</a>
+                    @else
                     <a href="{{ route('violator.create', $student->id) }}"
                         class="inline-flex items-center px-4 py-2 text-xs font-medium text-center text-white rounded-lg focus:ring-4 focus:outline-none bg-blue-600 hover:bg-blue-700 focus:ring-blue-800">
                         Add Violation
                     </a>
                     <a href="#"
-                        class="py-2 px-4 ms-2 text-xs font-medium  focus:outline-none rounded-lg border  focus:z-10 focus:ring-4  focus:ring-gray-700 bg-gray-800 text-gray-300 border-gray-600 hover:text-white hover:bg-gray-700">
+                        class="py-2 px-4 ms-2 text-xs font-medium focus:outline-none rounded-lg border  focus:z-10 focus:ring-4  focus:ring-gray-700 bg-gray-800 text-gray-300 border-gray-600 hover:text-white hover:bg-gray-700">
                         Show more</a>
+                    @endif
                 </div>
             </div>
         </div>
