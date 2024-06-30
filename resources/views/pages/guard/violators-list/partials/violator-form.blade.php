1f15a85e1eb08d8f5e2b85506c5b99a78df3c787
monciego <jerichobantiquete@gmail.com>
2024-06-30 23:47:42 +0800

    feat: show all input data (violators information) before submitting

--- resources/views/pages/guard/violators-list/partials/violator-form.blade.php
+++ resources/views/pages/guard/violators-list/partials/violator-form.blade.php
@@ -57,8 +57,148 @@
             </div>
         </div>
 
-        <x-primary-button class="mt-6 flex items-center justify-center w-full">
-            {{ __('Register as violator') }}
-        </x-primary-button>
+        <div x-data="{open:false}">
+            <x-primary-button type="button" x-on:click="open = true"
+                class="mt-6 flex items-center justify-center w-full">
+                {{ __('Register as violator') }}
+            </x-primary-button>
+
+            <div x-show="open" x-cloak class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity z-[500]"
+                aria-hidden="true"></div>
+
+            <div x-show="open" x-cloak class="fixed inset-0 w-screen overflow-y-auto z-[501]">
+                <div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+                    <div
+                        class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-3xl">
+                        <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+
+                            <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">
+                                Violator's Information
+                            </h3>
+                            <div class="grid grid-cols-1 mt-4 gap-4 sm:grid-cols-6">
+                                <div class="sm:col-span-2 ">
+                                    <p class="block font-medium text-sm text-gray-700">First Name</p>
+                                    <div id="display1"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <p class="block font-medium text-sm text-gray-700">Middle Name</p>
+                                    <div id="display2"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <p class="block font-medium text-sm text-gray-700">Last Name</p>
+                                    <div id="display3"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+
+                                <div class="sm:col-span-2">
+                                    <p class="block font-medium text-sm text-gray-700">Violation</p>
+                                    <div id="display4"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <p class="block font-medium text-sm text-gray-700">Violation Date</p>
+                                    <div id="display5"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+                                <div class="sm:col-span-2">
+                                    <p class="block font-medium text-sm text-gray-700">Violation Time</p>
+                                    <div id="display6"
+                                        class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm truncate">
+                                    </div>
+                                </div>
+
+                                <div class="col-span-6">
+                                    <p class="block font-medium text-sm text-gray-700">Comment</p>
+                                    <div class="py-2 px-3 mt-1 w-full border border-gray-300  rounded-md shadow-sm"
+                                        id="display7">
+                                    </div>
+                                </div>
+
+                                <div class="col-span-6">
+                                    <label for="remember_me" class="flex">
+                                        <input id="remember_me" type="checkbox" required
+                                            class="rounded mt-1 border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500"
+                                            name="remember">
+                                        <span class="ms-2 text-sm text-gray-700">{{ __('I confirm that I have
+                                            thoroughly reviewed the data pertaining to violators before submission. I
+                                            understand the importance of accuracy and take full responsibility for its
+                                            correctness.') }}</span>
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                            <button type="submit"
+                                class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 sm:ml-3 sm:w-auto">Confirm</button>
+                            <button type="button" x-on:click="open = false"
+                                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancel</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </form>
 </div>
+
+
+<script>
+    // Get references to the input fields and display elements
+        const inputs = [
+            document.getElementById("first_name"),
+            document.getElementById("middle_name"),
+            document.getElementById("last_name"),
+            document.getElementById("violation"),
+            document.getElementById("violation_date"),
+            document.getElementById("violation_time"),
+            document.getElementById("comment"),
+        ];
+        const displays = [
+            document.getElementById("display1"),
+            document.getElementById("display2"),
+            document.getElementById("display3"),
+            document.getElementById("display4"),
+            document.getElementById("display5"),
+            document.getElementById("display6"),
+            document.getElementById("display7"),
+        ];
+
+        // Initialize the data model
+        var array = [];
+        for (let i = 0; i < inputs.length; i++) {
+            let element = inputs[i].value;
+            array.push(element);
+        }
+
+        let dataModel = array;
+
+        // Function to update the UI with the current data model values
+        function updateUI() {
+            for (let i = 0; i < inputs.length; i++) {
+                dataModel[i] === "" ? displays[i].textContent = "No Data Entered" : displays[i].textContent = dataModel[i];
+            }
+        }
+
+        // Function to update the data model with the input values
+        function updateDataModel(index) {
+            dataModel[index] = inputs[index].value;
+            updateUI();
+        }
+
+        // Add event listeners for input changes and initial UI update
+        for (let i = 0; i < inputs.length; i++) {
+
+            inputs[i].addEventListener("input", (event) => {
+                updateDataModel(i);
+            });
+        }
+
+        // Initialize the UI with the current data model values
+        updateUI();
+</script>
